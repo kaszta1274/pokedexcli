ad6d9a418b0fdc31becfd7db0c48faf32cf37c0f
kaszta1274 <mateusz.gancarczyk04@gmail.com>
2026-08-18 14:30:40 +0200

    add help and exit

--- repl.go
+++ repl.go
@@ -1,10 +1,60 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		if err := command.callback(); err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
 func cleanInput(text string) []string {
-	text = strings.ToLower(strings.TrimSpace(text))
+	text = strings.ToLower(text)
 	return strings.Fields(text)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
